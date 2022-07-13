c687c01028d0e3bb8540730bd30249e8db8ea843
sue445 <sue445@sue445.net>
2022-07-13 22:27:20 +0900

    bundle gem --mit itamae-plugin-recipe-rust

---
+++ lib/itamae/plugin/recipe/rust.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "rust/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Rust
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/recipe/rust/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Rust
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
