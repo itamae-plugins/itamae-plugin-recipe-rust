76dcdbc24f3cd57f862364efc046f28ef6eaa8ab
sue445 <sue445@sue445.net>
2022-07-14 22:34:31 +0900

    Install sudo

--- spec/setup/default.rb
+++ spec/setup/default.rb
@@ -11,4 +11,10 @@ when "debian", "ubuntu"
 when "amazon"
   # `groupadd` isn't installed in https://hub.docker.com/_/amazonlinux
   package "shadow-utils"
+
+  [
+    "sudo"
+  ].each do |name|
+    package name
+  end
 end
